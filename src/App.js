ec715abb0a876a34183def86e2c8f1885e4ec91f
Derrice <derricewright@gmail.com>
2023-07-19 16:18:04 -0400

    Updating EditBudget

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 //Code to import Budget.js
 import Budget from './components/Budget';
 
@@ -11,8 +9,6 @@ import ExpenseTotal from './components/ExpenseTotal'
 import ExpenseItem from './components/ExpenseItem'
 import ExpenseList from './components/ExpenseList'
 import AllocationForm from './components/AllocationForm'
-
-
 import { AppProvider } from './context/AppContext';
 
 
@@ -20,7 +16,7 @@ const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <h1 className='mt-3'>AWD's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}  
                     
