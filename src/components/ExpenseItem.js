dd69f720e76e3c7f738c9f5e06fa004813e7fa9d
Derrice <derricewright@gmail.com>
2023-07-20 16:08:54 -0400

    Inc Dec Expense List v1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import {FcPlus} from 'react-icons/fc';
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -26,11 +27,26 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FcPlus onClick={event=> decreaseAllocation(props.name)}>- </FcPlus></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
